71813fbe3430a25576cf711c2a7842d5492ce4a9
Severyn Kurach <severynson.kurach@gmail.com>
2022-01-21 17:57:18 +0200

    developing second form in progres

--- src/Pages/Registration/SecondStep/SecondStep.js
+++ src/Pages/Registration/SecondStep/SecondStep.js
@@ -1,47 +1,71 @@
 import { Box, Grid, Button, TextField } from "@mui/material";
-import { AccountCircle, Collections } from "@mui/icons-material";
-import { blue } from "@mui/material/colors";
+import { AccountCircle, Collections, Instagram, Phone } from "@mui/icons-material";
+import { blue, red, teal } from "@mui/material/colors";
 const backgroundColor = blue[200];
-
+const imgButtonBgColor = red[300];
+const submitButtonBgColor = teal[500];
 
 const SecondStep = () => {
-
-    return <Box sx={{
+  return (
+    <Box
+      sx={{
         m: "0 auto",
+        justifyContent: "center",
         mt: "10%",
-        display: "flex",
+        display: "grid",
+        flexDirection: "column",
         bgcolor: backgroundColor,
         borderRadius: "30px",
         overflow: "hidden",
-      }}>
-        <Grid container width={"20px"} sx={{p: "20px"}}>
-            <Grid item>
-            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" label="With sx" variant="standard" />
+        p: 3
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "flex-end", mb: 2 }}>
+        <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="Name" variant="standard" />
       </Box>
-            </Grid>
-            <Grid item></Grid>
-            <Grid item>
-            <input
-  accept="image/*"
+      <Box sx={{ display: "flex", alignItems: "flex-end", mb: 2 }}>
+        <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="Surname" variant="standard" />
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "flex-end", mb: 2 }}>
+        <Phone sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="Phone" variant="standard" />
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "flex-end", mb: 2 }}>
+        <Instagram sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="Instagram" variant="standard" />
+      </Box>
 
-  style={{ display: 'none' }}
-  id="raised-button-file"
-  multiple
-  type="file"
-/>
-<label htmlFor="raised-button-file">
-  <Button variant="raised" component="span" startIcon={<Collections />}>
-    Uppload image
-  </Button>
-</label>
-            </Grid>
-            <Grid item></Grid>
-            <Grid item></Grid>
-            <Grid item></Grid>
-        </Grid>
+      <TextField id="input-with-sx" multiline rows={2} rowsMax={10} placeholder="A litle bit about yourself. Why do you want to learn?" variant="outlined" sx={{mb: 2}} />
+
+      <input
+        accept="image/*"
+        style={{ display: "none" }}
+        id="raised-button-file"
+        multiple
+        type="file"
+      />
+      <label htmlFor="raised-button-file">
+        <Button
+          variant="raised"
+          sx={{ bgcolor: imgButtonBgColor, mb: 2 }}
+          component="span"
+          startIcon={<Collections />}
+        >
+          Uppload avatar image
+        </Button>
+       </label> 
+        <Button
+          variant="raised"
+          sx={{ bgcolor: submitButtonBgColor }}
+          component="span"
+          startIcon={<Collections />}
+        >
+          Uppload avatar image
+        </Button>
     </Box>
+  );
 };
 
 export default SecondStep;
