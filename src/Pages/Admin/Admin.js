11d72b4fdacfbe42b2a1f1f374bf254fa27a52d1
Severyn Kurach <severynson.kurach@gmail.com>
2022-01-22 02:05:18 +0200

    + breakpoints to admin page

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,64 +1,168 @@
 import { Grid, Box, Typography, IconButton } from "@mui/material";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { blue, teal, red } from "@mui/material/colors";
-import {CheckBox, DeleteForever} from '@mui/icons-material';
+import { CheckBox, DeleteForever } from "@mui/icons-material";
 import React from "react";
-const  itemsBgColor = blue[200];
+import { useMediaQuery } from "@mui/material";
+const itemsBgColor = blue[200];
 const checkBoxColor = teal[500];
 const deniedColor = red[500];
 
 const DUMMY_USERLIST = [
-    {
-        name: "Severyn", surname: "Kurach", aboutYourself: "Hi, I'm Severyn! Want to study a lot! Please, let me in..."
-    },
-    {
-        name: "Roman", surname: "Nihto", aboutYourself: "Hi, I'm boring! Don't want to study at all! Let me in if you want))"
-    },
-    {
-        name: "Syava", surname: "Krasava", aboutYourself: "Hi, I'm very cool guy, but lazy! Want to study a lot! Please, let me in..."
-    },
+  {
+    name: "Severyn",
+    surname: "Kurach",
+    aboutYourself: "Hi, I'm Severyn! Want to study a lot! Please, let me in...",
+  },
+  {
+    name: "Roman",
+    surname: "Nihto",
+    aboutYourself:
+      "Hi, I'm boring! Don't want to study at all! Let me in if you want))",
+  },
+  {
+    name: "Syava",
+    surname: "Krasava",
+    aboutYourself:
+      "Hi, I'm very cool guy, but lazy! Want to study a lot! Please, let me in...",
+  },
 ];
 
 const Admin = () => {
-    return ( <Box sx={{margin: "0 auto"}}>
-        <Grid container sx={{width: "800px" ,m: "0 auto" ,marginTop: "9%"}} columns={4} >
-        <Grid item sx={{ bgcolor: itemsBgColor}}>
-                  <Box display="flex" sx={{width: "800px"}}>
-            <Box border="2px solid black" width="100px" p="10px">
-                <Typography variant="h6" fontWeight="bold">Name</Typography>
+  const [containerWidth, setContainerWidth] = useState(800);
+  const [typographyFont, setTypographyFont] = useState(20);
+  const [textFZ, setTextFZ] = useState(18);
+  const maxW400 = useMediaQuery("(max-width: 400px)");
+  const maxW500 = useMediaQuery("(max-width: 500px)");
+  const maxW600 = useMediaQuery("(max-width: 600px)");
+  const maxW700 = useMediaQuery("(max-width: 700px)");
+  const maxW800 = useMediaQuery("(max-width: 800px)");
+  const minW800 = useMediaQuery("(min-width: 800px)");
+
+  useEffect(() => {
+    if (maxW400) {
+      setContainerWidth(300);
+      setTypographyFont(10);
+      setTextFZ(10);
+    } else if (maxW500) {
+      setContainerWidth(400);
+      setTypographyFont(12);
+      setTextFZ(11);
+    } else if (maxW600) {
+      setContainerWidth(500);
+      setTypographyFont(12);
+      setTextFZ(11);
+    } else if (maxW700) {
+      setContainerWidth(600);
+      setTypographyFont(15);
+      setTextFZ(14);
+    } else if (maxW800) {
+      setContainerWidth(700);
+      setTypographyFont(17);
+      setTextFZ(16);
+    } else if (minW800) {
+      setContainerWidth(800);
+      setTypographyFont(20);
+      setTextFZ(17);
+    }
+  }, [maxW400, maxW500, maxW600, maxW700, maxW800, minW800]);
+
+  return (
+    <Box sx={{ margin: "0 auto" }}>
+      <Grid
+        container
+        sx={{ width: `${containerWidth}px`, m: "0 auto", marginTop: "9%" }}
+        columns={4}
+      >
+        <Grid item sx={{ bgcolor: itemsBgColor }}>
+          <Box display="flex" sx={{ width: `${containerWidth}px` }}>
+            <Box
+              border="2px solid black"
+              sx={{ width: `${containerWidth / 8}px` }}
+              p="10px"
+            >
+              <Typography sx={{ fontSize: typographyFont }} fontWeight="bold">
+                Name
+              </Typography>
             </Box>
-            <Box border="2px solid black" width="100px" p="10px">
-                <Typography variant="h6" fontWeight="bold">Surname</Typography>
+            <Box
+              border="2px solid black"
+              sx={{ width: `${containerWidth / 8}px` }}
+              p="10px"
+            >
+              <Typography sx={{ fontSize: typographyFont }} fontWeight="bold">
+                Surname
+              </Typography>
             </Box>
-            <Box border="2px solid black" width="400px" p="10px"> 
-                <Typography variant="h6" fontWeight="bold">About</Typography>
+            <Box
+              border="2px solid black"
+              sx={{ width: `${containerWidth / 2}px` }}
+              p="10px"
+            >
+              <Typography sx={{ fontSize: typographyFont }} fontWeight="bold">
+                About
+              </Typography>
             </Box>
-            <Box border="2px solid black" width="200px" p="10px">
-            <Typography variant="h6" fontWeight="bold">Accept / Denie</Typography>
+            <Box
+              border="2px solid black"
+              sx={{ width: `${containerWidth / 4}px` }}
+              p="10px"
+            >
+              <Typography sx={{ fontSize: typographyFont }} fontWeight="bold">
+                Accept / Denie
+              </Typography>
             </Box>
-            </Box>
-             </Grid>
-          {DUMMY_USERLIST.map((item, i) => (
-              <Grid item key={i} sx={{ bgcolor: itemsBgColor}}>
-                  <Box display="flex" sx={{width: "800px" ,m: "0 auto"}}>
-            <Box border="2px solid black" width="100px" p="10px">
-                {item.name}
-            </Box>
-            <Box border="2px solid black" width="100px" p="10px">
-                {item.surname}
-            </Box>
-            <Box border="2px solid black" width="400px" p="10px">
-                {item.aboutYourself}
-            </Box>
-            <Box border="2px solid black" width="200px" p="10px" sx={{display: 'flex', justifyContent: 'space-evenly'}}>
-            <IconButton sx={{bgcolor: checkBoxColor}}><CheckBox /></IconButton>
-            <IconButton sx={{bgcolor: deniedColor}}><DeleteForever /></IconButton>
-            </Box>
-            </Box>
-             </Grid>
-          ))}
+          </Box>
         </Grid>
-      </Box>);
+        {DUMMY_USERLIST.map((item, i) => (
+          <Grid item key={i} sx={{ bgcolor: itemsBgColor }}>
+            <Box
+              display="flex"
+              sx={{ width: `${containerWidth}px`, m: "0 auto" }}
+            >
+              <Box
+                border="2px solid black"
+                sx={{ width: `${containerWidth / 8}px`, fontSize: textFZ }}
+                p="10px"
+              >
+                {item.name}
+              </Box>
+              <Box
+                border="2px solid black"
+                sx={{ width: `${containerWidth / 8}px`, fontSize: textFZ }}
+                p="10px"
+              >
+                {item.surname}
+              </Box>
+              <Box
+                border="2px solid black"
+                sx={{ width: `${containerWidth / 2}px`, fontSize: textFZ }}
+                p="10px"
+              >
+                {item.aboutYourself}
+              </Box>
+              <Box
+                border="2px solid black"
+                sx={{
+                  width: `${containerWidth / 4}px`,
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                }}
+                p="10px"
+              >
+                <IconButton sx={{ bgcolor: checkBoxColor, width: `${containerWidth / 17}px`, height: `${containerWidth / 17}px` }}>
+                  <CheckBox />
+                </IconButton>
+                <IconButton  sx={{ bgcolor: deniedColor, width: `${containerWidth / 17}px`, height: `${containerWidth / 17}px` }}>
+                  <DeleteForever />
+                </IconButton>
+              </Box>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Admin;
