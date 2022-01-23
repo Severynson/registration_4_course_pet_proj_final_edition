43026434cee80214fd52884d59562f8a2674f298
Severyn Kurach <severynson.kurach@gmail.com>
2022-01-23 02:28:41 +0200

    Firebase connected and works+

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,15 +1,26 @@
 import LoginPasswordForm from "../../UI/EmailPasswordForm";
-// import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+let users;
 
 const LogIn = () => {
+    const usersCollectionRef = collection(db, "users");
+    useEffect(() => {
+        const getUsers = async() => {
+          const data = await getDocs(usersCollectionRef);
+          console.log(data);
+        users = (data.docs.map((doc) => ({
+              ...doc.data()
+          })))
+        };
+        getUsers();
+    }, [])
+
     const handleLoginAndPassword = async (e) => {
         const {email, password} = e;
         console.log(e);
-        // const auth = getAuth();
-        // console.log(auth);
-        // const signIn = await signInWithEmailAndPassword(auth, email, password);
-        // console.log(signIn)
-    
+        users.find(item => item.email === email && item.password === password);
     };
 
     return (<LoginPasswordForm type="login" sendData={handleLoginAndPassword} />);
