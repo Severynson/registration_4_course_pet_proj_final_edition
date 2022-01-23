2a650983ddbd6d8e7b62caa4866e86e88cd438ca
Severyn Kurach <severynson.kurach@gmail.com>
2022-01-23 02:56:34 +0200

    +redux

---
+++ src/store/slices/userSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        logIn(state, action) {
+            state = action.payload; 
+        },
+        logOut(state) {
+            state = initialState;
+        },
+    }
+});
+
+export const userAction = userSlice.actions;
